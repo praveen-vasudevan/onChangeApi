e35f8093fd5f632565f38582681de8693086ed26
praveen-vasudevan <praveenvasudevangit@gmail.com>
2022-01-26 23:31:16 +0530

    comments added

--- project-ram/src/Components/Home.jsx
+++ project-ram/src/Components/Home.jsx
@@ -4,15 +4,26 @@ import '../Components/Home.css';
 
 
 const Home = () => {
-
+    //This state will hold the final data after submission. We can send this data to another 
+    //component by props and render there...
     const[datas, setDatas] = useState([]);
+
+    //This state will hold the suggested locations respective to onchange location input
     const[suggestions, setSuggestions] = useState([]);
 
+    //This two state holds the location and type value
     const[location, setLocation] = useState('')
     const[type, setType] = useState('')
-    const[submit, setSubmit] = useState(false)
-    
 
+    //This state is to check whether the form is submitted or not
+    const[submit, setSubmit] = useState(false)
+
+
+
+    /*
+        This function will be called after clicking the submit button. This function
+        fetch for final data and store it in 'data-state'. 
+    */ 
     const submitData = async () => {
 
         const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
@@ -26,6 +37,12 @@ const Home = () => {
 
     }
 
+    /*
+        This function will be called whenever there is a change in location value. 
+        The response will be stored in the 'suggestion-state' 
+
+    */
+
     const fetchData = async () => {
 
         const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
@@ -38,6 +55,10 @@ const Home = () => {
         console.log(suggestions)
     }
 
+    /*
+        This useEffect hook will be executed at the beginning and is also execute when there
+        is a change in 'location-state' and 'submit-state'.
+    */
     useEffect(() => {
 
         if(submit){
@@ -60,18 +81,22 @@ const Home = () => {
     }, [location, submit]);
 
 
+    //This function will set the location value in 'location-state'
     const onChangeHandlerLocation = (e) => {
         setLocation(e.target.value)
     }
 
+    //This function will set the type value in 'type-state'
     const onChangeHandlerType = (e) => {
         setType(e.target.value)
     }
 
+    //This function will change location value by suggested value(which is clicked by the user)
     const onClickHandlerSuggestion = (e) => {
         setLocation(e.target.innerHTML)
     }
 
+    //This function is for form validation and submission. It will change the 'submit-state' as true.
     const submitEventHandler = (e) => {
         e.preventDefault()
 
@@ -113,6 +138,8 @@ const Home = () => {
             />
             <div className="suggestion-container">
                 {
+                    //This is where we are rendering the suggested locations. If the suggestion is null
+                    //we render nothing... 
                     (suggestions !== [])?  suggestions.map(suggestion => (
 
                         <div onClick={onClickHandlerSuggestion}  key = {suggestion.id} className="suggestion">{suggestion.userId}</div>
